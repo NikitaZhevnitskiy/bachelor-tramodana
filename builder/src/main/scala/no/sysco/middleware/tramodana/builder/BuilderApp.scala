dc0156e71ecce9317e8e6d46e7e27255396fd2c6
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2018-03-23 17:28:51 +0100

    prepare tree build

--- builder/src/main/scala/no/sysco/middleware/tramodana/builder/BuilderApp.scala
+++ builder/src/main/scala/no/sysco/middleware/tramodana/builder/BuilderApp.scala
@@ -1,6 +1,6 @@
 package no.sysco.middleware.tramodana.builder
 
-import java.util.Properties
+import java.util.{Properties, UUID}
 import java.util.concurrent.CountDownLatch
 
 import org.apache.kafka.common.serialization.Serdes
@@ -9,12 +9,12 @@ import org.apache.kafka.streams.kstream.KStream
 import spray.json.JsonParser
 
 
-object BuilderApp extends App {
+object BuilderApp extends App with JsonSpanProtocol {
 //  val configs = Configs.buildServerConfig()
 //  val kafkaTraceConsumer = new KafkaTraceConsumer(configs.kafkaConfig.bootstrapServers)
 //  kafkaTraceConsumer.run()
-val props = new Properties
-  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "experiments")
+  val props = new Properties
+  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "experiments"+UUID.randomUUID().toString)
   props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
   props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String.getClass)
   props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String.getClass)
@@ -22,23 +22,22 @@ val props = new Properties
 
   val builder = new StreamsBuilder
 
-  // 1 - stream from kafka
-  val source: KStream[java.lang.String, String] = builder.stream[String, String]("spans-json-original")
+//   1 - stream from kafka
+  val originalSource: KStream[java.lang.String, String] = builder.stream[String, String]("spans-json-original")
 
 
-  // to TRACES topic
-  source
+  // to TRACES topic [trace_id, List[Span]]
+  originalSource
     .groupByKey()
     // concatenate
     .reduce((value1, value2)=>value1+","+ value2)
     .toStream
     .mapValues(v=>s"[$v]")
     .to("traces")
-  //    .foreach((k,v)=>println(k+s"values: $v"))
 
 
-  // to SPANS topic
-  source
+  // to SPANS topic [span_id, Span]
+  originalSource
     .map[java.lang.String, String]( (_,value) => {
     val span: Span = JsonParser(value).convertTo[Span]
     val traceId = java.lang.String.valueOf(span.spanId)
@@ -47,6 +46,16 @@ val props = new Properties
     new KeyValue[java.lang.String,String](traceId, value)
   })
     .to("spans")
+
+
+  // to Processed-traces [trace_id, Tree[Span] ]
+  val tracesSource: KStream[String, String] = builder.stream[String,String]("traces")
+  println("HERE: ")
+  tracesSource.peek((k, v)=>{
+    val list: List[Span] = JsonParser(v).convertTo[List[Span]]
+    println(list)
+//    list.foreach(span=>println(span.parentId))
+  })
 
   val topology = builder.build
   val streams = new KafkaStreams(topology, props)
