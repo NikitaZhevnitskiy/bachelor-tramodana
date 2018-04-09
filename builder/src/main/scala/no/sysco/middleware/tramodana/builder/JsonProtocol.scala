7b25471d9503e6857afd783ebe68f867ce209dc4
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2018-04-09 14:47:13 +0200

    refactor n-ary Tree

--- builder/src/main/scala/no/sysco/middleware/tramodana/builder/JsonProtocol.scala
+++ builder/src/main/scala/no/sysco/middleware/tramodana/builder/JsonProtocol.scala
@@ -1,7 +1,6 @@
 package no.sysco.middleware.tramodana.builder
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-
 import spray.json.{DefaultJsonProtocol, JsonParser, RootJsonFormat}
 
 final case class Trace(traceId:Option[String], spans: Option[List[Span]])
@@ -44,11 +43,16 @@ final case class Ref(
                       traceId: String,
                       spanId: String)
 
+final case class SpanTree(
+                           operationName: String,
+                           value: Span,
+                           parent: Option[String] = None,
+                           children: List[SpanTree] = List.empty)
+
 
 
 
 trait JsonSpanProtocol extends SprayJsonSupport with DefaultJsonProtocol {
-
 
   implicit def spanFormat: RootJsonFormat[Span] =
     jsonFormat(Span, "trace_id", "span_id", "span_hash","duration", "flags", "logs", "operation_name","parent_id", "process", "refs", "start_time", "tags")
@@ -57,6 +61,8 @@ trait JsonSpanProtocol extends SprayJsonSupport with DefaultJsonProtocol {
   implicit def logFieldFormat: RootJsonFormat[Field] = jsonFormat(Field, "key", "value_type", "value_string","value_bool","value_long","value_double","value_binary")
   implicit def spanProcessFormat: RootJsonFormat[Process] = jsonFormat(Process,"service_name", "tags")
   implicit def spanRefFormat: RootJsonFormat[Ref] = jsonFormat(Ref,"ref_type", "trace_id","span_id")
+//  implicit def spanTree: RootJsonFormat[SpanTree] = jsonFormat4(SpanTree, "operation_name", "value", "parent","children")
+  implicit def spanTree: RootJsonFormat[SpanTree] = jsonFormat4(SpanTree)
 }
 
 object MakeFun extends App with JsonSpanProtocol {
