e21cb2cad2fe282be2779d5185864dc7a9bb7952
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2018-04-13 16:02:41 +0200

    preStart() create all topics before topology start

--- builder/src/main/scala/no/sysco/middleware/tramodana/builder/BuilderApp.scala
+++ builder/src/main/scala/no/sysco/middleware/tramodana/builder/BuilderApp.scala
@@ -4,10 +4,14 @@ import java.util.concurrent.CountDownLatch
 import java.util.{Properties, UUID}
 
 import no.sysco.middleware.tramodana.builder.model.SpanTreeBuilder
+import org.apache.kafka.clients.admin.{AdminClient, AdminClientConfig, NewTopic}
+import org.apache.kafka.common.errors.TopicExistsException
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams._
 import org.apache.kafka.streams.kstream.KStream
 
+import scala.collection.JavaConverters._
+import scala.concurrent.ExecutionException
 
 
 object BuilderApp extends App with JsonSpanProtocol {
@@ -20,19 +24,21 @@ object BuilderApp extends App with JsonSpanProtocol {
   val TRACE_ID_ROOT_OPERATION = "trace-id-root-operation"
   val TRACE_ID_SEQ_OPERATIONS = "trace-id-seq-operations"
 
+  start()
 
 
-  val props = getProps()
-  val builder = new StreamsBuilder
-  val topology = buildTopology(builder)
-  val streams = new KafkaStreams(topology, props)
+  def start(): Unit = {
+    val props = getProps()
+    preStart(props)
 
-  addShutdownHook(streams, new CountDownLatch(1))
+    val builder = new StreamsBuilder
+    val topology = buildTopology(builder)
+    val streams = new KafkaStreams(topology, props)
 
+    addShutdownHook(streams, new CountDownLatch(1))
+  }
 
-
-
-  def addShutdownHook(streams: KafkaStreams, latch: CountDownLatch): Unit ={
+  def addShutdownHook(streams: KafkaStreams, latch: CountDownLatch): Unit = {
     // attach shutdown handler to catch control-c
     Runtime.getRuntime.addShutdownHook(new Thread("streams-experiments-shutdown-hook") {
       override def run(): Unit = {
@@ -50,9 +56,11 @@ object BuilderApp extends App with JsonSpanProtocol {
     }
     System.exit(0)
   }
-  def getProps():Properties = {
+
+  def getProps(): Properties = {
     val props = new Properties
-    props.put(StreamsConfig.APPLICATION_ID_CONFIG, this.getClass.getSimpleName+UUID.randomUUID().toString)
+    props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    props.put(StreamsConfig.APPLICATION_ID_CONFIG, this.getClass.getSimpleName + UUID.randomUUID().toString)
     props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
     props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String.getClass)
     props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String.getClass)
@@ -60,8 +68,7 @@ object BuilderApp extends App with JsonSpanProtocol {
   }
 
 
-
-  def buildTopology(builder: StreamsBuilder):Topology = {
+  def buildTopology(builder: StreamsBuilder): Topology = {
     import spray.json._
 
     //   1 - stream from kafka
@@ -72,26 +79,26 @@ object BuilderApp extends App with JsonSpanProtocol {
     originalSource
       .groupByKey()
       // concatenate
-      .reduce((value1, value2)=>value1+","+ value2)
+      .reduce((value1, value2) => value1 + "," + value2)
       .toStream
-      .mapValues(v=>s"[$v]")
+      .mapValues(v => s"[$v]")
       .to(TRACES)
 
 
     // 3 to SPANS topic [span_id, Span]
     originalSource
-      .map[java.lang.String, String]( (_,value) => {
+      .map[java.lang.String, String]((_, value) => {
       val span: Span = JsonParser(value).convertTo[Span]
       val traceId = java.lang.String.valueOf(span.spanId)
       println(span.spanId)
       println(traceId)
-      new KeyValue[java.lang.String,String](traceId, value)
+      new KeyValue[java.lang.String, String](traceId, value)
     })
       .to(SPANS)
 
     // 4 Processed-traces [trace_id, Tree[Span] ]
     val tracesSource: KStream[String, String] =
-      builder.stream[String,String](TRACES)
+      builder.stream[String, String](TRACES)
 
 
     tracesSource.mapValues(v => {
@@ -106,23 +113,27 @@ object BuilderApp extends App with JsonSpanProtocol {
     val processedTracesSource: KStream[String, String] =
       builder.stream[String, String](PROCESSED_TRACES)
 
+    // NB! Topic should be manually created
     // 6 Trace-id-root-operation [trace_id, String]
-    processedTracesSource
+    val traceIdRootOperationStream = processedTracesSource
       .mapValues(jsonTree => {
         val tree = JsonParser(jsonTree).convertTo[SpanTree]
         tree.value.operationName
       })
-        .to(TRACE_ID_ROOT_OPERATION)
+      .through(TRACE_ID_ROOT_OPERATION)
 
+    // NB! Topic should be manually created
     // 7 trace-id-seq-operations [trace_id, Seq[Span]]
-    processedTracesSource
-        .mapValues(jsonTree => {
-          val tree = JsonParser(jsonTree).convertTo[SpanTree]
-          val seq = SpanTreeBuilder.getSequence(tree)
-          seq.toJson.toString
-        })
-        .to(TRACE_ID_SEQ_OPERATIONS)
+    val traceIdSeqOperation = processedTracesSource
+      .mapValues(jsonTree => {
+        val tree = JsonParser(jsonTree).convertTo[SpanTree]
+        val seq = SpanTreeBuilder.getSequence(tree)
+        seq.toJson.toString
+      })
+      .through(TRACE_ID_SEQ_OPERATIONS)
 
+
+    // 8 Join [6 & 7] -> [Trace-id-root-operation, List[Seq[Span]]]
 
     // build topology
     builder.build
@@ -130,43 +141,37 @@ object BuilderApp extends App with JsonSpanProtocol {
   }
 
 
-
   /**
     * CREATE TOPICS in PRE_START stage
-    * */
+    **/
 
-//  // with existing props
-//  def createAdminClient(properties: Properties): AdminClient = {
-//    val configs = {
-//      properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG))
-//      properties
-//    }
-//    AdminClient.create(configs)
-//  }
-//
-//  def preStart(): Unit = {
-//    val adminClient: AdminClient = createAdminClient()
-//    val newTopics =
-//      Seq(
-//        new NewTopic(SPANS_JSON_ORIGINAL, 1, 1),
-//        new NewTopic(SPANS, 1, 1),
-//        new NewTopic(TRACES, 1, 1),
-//        new NewTopic(PROCESSED_TRACES, 1, 1),
-//        new NewTopic(TRACE_ID_ROOT_OPERATION, 1, 1),
-//        new NewTopic(TRACE_ID_SEQ_OPERATIONS, 1, 1)
-//      )
-//    try {
-//      val result = adminClient.createTopics(newTopics.asJava)
-//      result.all().get()
-//    } catch {
-//      case e: ExecutionException =>
-//        e.getCause match {
-//          case _: TopicExistsException =>
-//            println(s"Topics ${newTopics.map(_.name())} already exist")
-//        }
-//      case e: Throwable => throw e
-//    }
-//  }
+  //  // with existing props
+
+  //
+  def preStart(properties: Properties): Unit = {
+
+    val adminClient: AdminClient = AdminClient.create(properties)
+    val newTopics =
+      Seq(
+        new NewTopic(SPANS_JSON_ORIGINAL, 1, 1),
+        new NewTopic(SPANS, 1, 1),
+        new NewTopic(TRACES, 1, 1),
+        new NewTopic(PROCESSED_TRACES, 1, 1),
+        new NewTopic(TRACE_ID_ROOT_OPERATION, 1, 1),
+        new NewTopic(TRACE_ID_SEQ_OPERATIONS, 1, 1)
+      )
+    try {
+      val result = adminClient.createTopics(newTopics.asJava)
+      result.all().get()
+    } catch {
+      case e: ExecutionException =>
+        e.getCause match {
+          case _: TopicExistsException =>
+            println(s"Topics ${newTopics.map(_.name())} already exist")
+        }
+      case e: Throwable => throw e
+    }
+  }
 }
 
 
