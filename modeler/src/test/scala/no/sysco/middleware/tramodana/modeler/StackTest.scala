e11df8ac640dea58f6707d7712c12f549bf369eb
Clement Marescaux <marcle15@student.westerdals.no>
2018-04-19 15:27:21 +0200

    Test stub for Stack

---
+++ modeler/src/test/scala/no/sysco/middleware/tramodana/modeler/StackTest.scala
@@ -0,0 +1,43 @@
+package no.sysco.middleware.tramodana.modeler
+
+import org.scalatest.{BeforeAndAfterEach, FlatSpec, FunSuite, Matchers}
+
+class StackTest extends FlatSpec with Matchers {
+
+  def beforeEach() {
+   var stack = new Stack[Int](List(1,2,3,4))
+  }
+
+  test("testPushAll") {
+
+  }
+
+  test("testNonEmpty") {
+
+  }
+
+  test("testEmpty") {
+
+  }
+
+  test("testPop") {
+
+  }
+
+  test("testList") {
+
+  }
+
+  test("testList_$eq") {
+
+  }
+
+  test("testPush") {
+
+  }
+
+  test("testPeek") {
+
+  }
+
+}
