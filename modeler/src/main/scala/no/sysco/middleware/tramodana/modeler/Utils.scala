1d19ea4368e8b5348073685e1ccd7bf390ce44e4
Clement Marescaux <marcle15@student.westerdals.no>
2018-04-25 15:20:38 +0200

    Move example writer method to Utils class

--- modeler/src/main/scala/no/sysco/middleware/tramodana/modeler/Utils.scala
+++ modeler/src/main/scala/no/sysco/middleware/tramodana/modeler/Utils.scala
@@ -1,8 +1,10 @@
 package no.sysco.middleware.tramodana.modeler
 
+import java.io.{BufferedWriter, File, FileWriter}
+
 object Utils {
  var counter : Int = 1
-    def createNode(parentId: String, op: String = "op", procId: String = "proc", children: List[Node] = Nil): Node ={
+    def createNode(parentId: String = "0", op: String = "op", procId: String = "proc", children: List[Node] = List.empty ): Node ={
       val id = counter.toString
       counter += 1
         new Node(
@@ -15,13 +17,20 @@ object Utils {
         )
     }
 
-
+   def writeToExampleDir(content: String, fileNameWithoutExt: String): Unit = {
+    val file = new File(s"examples/output_for_modeler/$fileNameWithoutExt.bpmn")
+    val bufferedWriter = new BufferedWriter(new FileWriter(file))
+    bufferedWriter.write(content)
+    bufferedWriter.close()
+  }
 }
+
 case class Node(operationName: String,
                   processId: String,
                   children: List[Node],
                   parentId: String) extends BpmnParsable with NodeUtils {
-    override def getChildren: List[BpmnParsable] = children
+
+  override def getChildren: List[BpmnParsable] = children
     override def getParentId: String = parentId
     override def setParentId(id: String): BpmnParsable = copy(parentId = id)
     override def getOperationName: String = operationName
@@ -29,6 +38,7 @@ case class Node(operationName: String,
 
   override def addChild(node: Node): Node = this.copy( children = node :: children)
   override def addChildren(nodes: List[Node]): Node = this.copy(children = nodes ::: children)
+
 }
 
 trait NodeUtils{
