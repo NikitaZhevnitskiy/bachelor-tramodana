a66a9c84b478fb987add8bcfe732ff0878b1273e
Clement Marescaux <marcle15@student.westerdals.no>
2018-04-25 15:18:17 +0200

    Add default tree printing function

--- modeler/src/main/scala/no/sysco/middleware/tramodana/modeler/BpmnParsable.scala
+++ modeler/src/main/scala/no/sysco/middleware/tramodana/modeler/BpmnParsable.scala
@@ -6,4 +6,22 @@ trait BpmnParsable{
   def getChildren: List[BpmnParsable]
   def getOperationName: String
   def getProcessId: String
+  def printPretty: Unit = printPrettyIter("",true)
+  private def printPrettyIter(ind: String, last: Boolean): Unit = {
+    var indent = ind
+    print(indent)
+
+    if(last) {
+      print("\\-")
+      indent ++= "  "
+    } else {
+      print("|-")
+      indent ++= "| "
+    }
+    println(getProcessId)
+    for( i <- getChildren.indices)
+    {
+      getChildren(i).printPrettyIter(indent, i == (getChildren.size -1) )
+    }
+  }
 }
