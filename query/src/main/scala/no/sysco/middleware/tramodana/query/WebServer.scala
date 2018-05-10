6aedfd35152a1d70a599dda9814373f9f9e61a86
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2018-05-10 17:36:55 +0200

    poc query module

--- query/src/main/scala/no/sysco/middleware/tramodana/query/WebServer.scala
+++ query/src/main/scala/no/sysco/middleware/tramodana/query/WebServer.scala
@@ -1,36 +1,104 @@
 package no.sysco.middleware.tramodana.query
 
+import java.util.Properties
+import java.util.concurrent.CountDownLatch
+
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.server.Directives._
+import no.sysco.middleware.tramodana.schema.Topic
+import org.apache.kafka.clients.admin.AdminClientConfig
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.common.utils.Bytes
+import org.apache.kafka.streams.kstream.Materialized
+import org.apache.kafka.streams.state.{KeyValueIterator, KeyValueStore, QueryableStoreTypes, ReadOnlyKeyValueStore}
+import org.apache.kafka.streams.{KafkaStreams, StreamsBuilder, StreamsConfig, Topology}
 
+import scala.collection.mutable.ListBuffer
 import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
-
-object WebServer {
-  def main(args: Array[String]) {
-
-    implicit val system : ActorSystem= ActorSystem("my-system")
-    implicit val materializer : ActorMaterializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
-    implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+import scala.collection.JavaConverters._
+import scala.collection.mutable
 
 
-    val route =
-      path("hello") {
-        get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
-        }
+object WebServer extends App {
+
+  // Akka
+  implicit val system : ActorSystem= ActorSystem("my-system")
+  implicit val materializer : ActorMaterializer = ActorMaterializer()
+  // needed for the future flatMap/onComplete in the end
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
+  // Streams
+  // prestart
+  val STORAGE : String = s"storage"
+  val props = getProps("example2", "localhost:9092")
+  Topic.preStart(props)
+  // run streams
+  val builder = new StreamsBuilder
+  val topology = buildTopology(builder)
+  val streams = new KafkaStreams(topology, props)
+
+  val route =
+    path("key") {
+      get {
+        getValueByKey("hello")
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Get value by key via akka-http</h1>"))
       }
+    } ~
+    path("all"){
+      get {
+        getAllValues()
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Get all values via akka-http</h1>"))
+      }
+    }
 
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8888)
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 8888)
+  // shutdown hook
+  Topic.addShutdownHook(streams, new CountDownLatch(1))
 
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
+
+  def getValueByKey(key: String): Unit =  {
+    val storeType = QueryableStoreTypes.keyValueStore[String, String]()
+    val keyValueStore: ReadOnlyKeyValueStore[String, String] = streams.store(STORAGE,storeType)
+    println(keyValueStore.get(key))
+  }
+
+  def getAllValues():Unit = {
+    val storeType = QueryableStoreTypes.keyValueStore[String, String]()
+    val keyValueStore: ReadOnlyKeyValueStore[String, String] = streams.store(STORAGE,storeType)
+
+    val it: KeyValueIterator[String, String] = keyValueStore.all()
+    var list = new ListBuffer[String]()
+
+    while (it.hasNext) {
+      val nextKV = it.next
+      println(s"${nextKV.key} : ${nextKV.value}")
+      list += nextKV.value
+    }
+
+    println(s"FINAL : ${list.toList}")
+  }
+
+
+  def getProps(appName: String, kafkaBootstrapServer: String): Properties = {
+    val props = new Properties
+    props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapServer)
+    props.put(StreamsConfig.APPLICATION_ID_CONFIG, appName)
+    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapServer)
+    props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String.getClass)
+    props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String.getClass)
+    props.put(StreamsConfig.APPLICATION_SERVER_CONFIG, "localhost:8888")
+    props
+  }
+
+  def buildTopology(builder: StreamsBuilder): Topology = {
+    val oNameStorageXml =
+      builder.table(
+        Topic.ROOT_OPERATION_BPMN_XML,
+        Materialized.as[String, String, KeyValueStore[Bytes, Array[Byte]]](STORAGE))
+    builder.build()
   }
 }
--- schema/src/main/scala/no/sysco/middleware/tramodana/schema/Topic.scala
+++ schema/src/main/scala/no/sysco/middleware/tramodana/schema/Topic.scala
@@ -1,9 +1,18 @@
 package no.sysco.middleware.tramodana.schema
 
+import java.util.Properties
+import java.util.concurrent.CountDownLatch
+
+import org.apache.kafka.clients.admin.{AdminClient, NewTopic}
+import org.apache.kafka.common.errors.TopicExistsException
+import org.apache.kafka.streams.KafkaStreams
+
+import scala.concurrent.ExecutionException
+import scala.collection.JavaConverters._
+
 object Topic {
   // topic names
   final val SPANS_ORIGINAL_TOPIC: String = "spans-json-original"
-
   final val SPANS_JSON_ORIGINAL = "spans-json-original"
   final val SPANS = "spans"
   final val TRACES = "traces"
@@ -13,12 +22,64 @@ object Topic {
   final val ROOT_SPAN_SEQ_SPAN = "root-span-seq-span"
   final val ROOT_OPERATION_LIST_SEQ_SPAN = "root-operation-list-seq-span"
   final val ROOT_OPERATION_SET_SEQ_SPANS = "root-operation-set-seq-spans"
-  final val ROOT_OPERATION_SET_SEQ_SPANS_TABLE = "root-operation-set-seq-spans-table"
   final val ROOT_OPERATION_SET_SPAN_TREES = "root-operation-set-spantree"
-  final val ROOT_OPERATION_SET_SPAN_TREES_TABLE = "root-operation-set-spantree-table"
   final val ROOT_OPERATION_BPMN_XML = "root-operation-bpmn-xml"
+  // tables
+  final val ROOT_OPERATION_SET_SEQ_SPANS_TABLE = "root-operation-set-seq-spans-table"
+  final val ROOT_OPERATION_SET_SPAN_TREES_TABLE = "root-operation-set-spantree-table"
 
 
   // utils
   final val EMPTY_KEY: String = ""
+
+  def preStart(properties: Properties): Unit = {
+
+    val adminClient: AdminClient = AdminClient.create(properties)
+    val newTopics =
+      Seq(
+
+        new NewTopic(Topic.SPANS_ORIGINAL_TOPIC, 1, 1),
+        new NewTopic(Topic.SPANS_JSON_ORIGINAL, 1, 1),
+        new NewTopic(Topic.SPANS, 1, 1),
+        new NewTopic(Topic.TRACES, 1, 1),
+        new NewTopic(Topic.PROCESSED_TRACES, 1, 1),
+        new NewTopic(Topic.TRACE_ID_ROOT_OPERATION, 1, 1),
+        new NewTopic(Topic.TRACE_ID_SEQ_SPAN, 1, 1),
+        new NewTopic(Topic.ROOT_SPAN_SEQ_SPAN, 1, 1),
+        new NewTopic(Topic.ROOT_OPERATION_LIST_SEQ_SPAN, 1, 1),
+        new NewTopic(Topic.ROOT_OPERATION_SET_SEQ_SPANS, 1, 1),
+        new NewTopic(Topic.ROOT_OPERATION_SET_SPAN_TREES, 1, 1),
+        new NewTopic(Topic.ROOT_OPERATION_BPMN_XML, 1, 1)
+      )
+    try {
+      val result = adminClient.createTopics(newTopics.asJava)
+      result.all().get()
+    } catch {
+      case e: ExecutionException =>
+        e.getCause match {
+          case _: TopicExistsException =>
+            println(s"Topics ${newTopics.map(_.name())} already exist")
+        }
+      case e: Throwable => throw e
+    }
+  }
+
+  def addShutdownHook(streams: KafkaStreams, latch: CountDownLatch): Unit = {
+    // attach shutdown handler to catch control-c
+    Runtime.getRuntime.addShutdownHook(new Thread("streams-experiments-shutdown-hook") {
+      override def run(): Unit = {
+        streams.close()
+        latch.countDown()
+      }
+    })
+
+    try {
+      streams.start()
+      latch.await()
+    } catch {
+      case e: Throwable =>
+        System.exit(1)
+    }
+    System.exit(0)
+  }
 }
