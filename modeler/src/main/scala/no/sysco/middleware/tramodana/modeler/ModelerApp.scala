33e39d0e5313f1edcb671e9096b037609d2a62d8
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2018-05-12 17:44:58 +0200

    refactor modeler app

--- modeler/src/main/scala/no/sysco/middleware/tramodana/modeler/ModelerApp.scala
+++ modeler/src/main/scala/no/sysco/middleware/tramodana/modeler/ModelerApp.scala
@@ -3,42 +3,15 @@ package no.sysco.middleware.tramodana.modeler
 import java.util.Properties
 import java.util.concurrent.CountDownLatch
 
-
 import no.sysco.middleware.tramodana.modeler.util.AppConfig
-import no.sysco.middleware.tramodana.schema.Topic
-import org.apache.kafka.clients.admin.{AdminClient, AdminClientConfig, NewTopic}
-import org.apache.kafka.common.errors.TopicExistsException
+import no.sysco.middleware.tramodana.schema.{Topic, TramodanaKafkaAdministrator}
+import org.apache.kafka.clients.admin.AdminClientConfig
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.kstream.KStream
-import org.apache.kafka.streams.{KafkaStreams, KeyValue, StreamsBuilder, StreamsConfig, Topology, _}
-import spray.json.DeserializationException
-
-import scala.collection.JavaConverters._
-import scala.concurrent.ExecutionException
-import scala.io.Source
+import org.apache.kafka.streams._
 
 
 object ModelerApp extends App {
-
-  //  def run: Unit = {
-  //    val INPUT_FILES_DIRECTORY = "examples/input_for_modeler"
-  //
-  //    val jsonSource: String = Source
-  //      .fromFile(s"$INPUT_FILES_DIRECTORY/ROOT_OPERATION_SET_SPAN_TREES.json")
-  //      .getLines
-  //      .mkString
-  //
-  //    val parser: JsonToSpanNodeParser = new JsonToSpanNodeParser()
-  //    val tree:  Option[BpmnParsable] = parser.parse(jsonSource)
-  //
-  //    val bpmnCreator = tree match {
-  //      case Some(parsable) => new BpmnCreator(parsable, "00 test")
-  //      case None => throw new Exception("SpanTrees could not be merged")
-  //    }
-  //
-  //    val bpmnXmlString: String = bpmnCreator.getBpmnXmlStr.get
-  //    println(bpmnXmlString)
-  //  }
 
   run
 
@@ -46,13 +19,14 @@ object ModelerApp extends App {
 
     val modelerConfig: AppConfig.ModelerConfig = AppConfig.load()
     val props = getProps(modelerConfig.name, modelerConfig.kafka.bootstrapServers)
-    preStart(props)
+
+    TramodanaKafkaAdministrator.preStart(props)
 
     val builder = new StreamsBuilder
     val topology = buildTopology(builder)
     val streams = new KafkaStreams(topology, props)
 
-    addShutdownHook(streams, new CountDownLatch(1))
+    TramodanaKafkaAdministrator.addShutdownHook(streams, new CountDownLatch(1), modelerConfig.name)
   }
 
   def getProps(appName: String, kafkaBootstrapServer: String): Properties = {
@@ -66,7 +40,6 @@ object ModelerApp extends App {
   }
 
   def buildTopology(builder: StreamsBuilder): Topology = {
-    import spray.json._
 
     val spanTreesSource: KStream[String, String] =
       builder.stream[String, String](Topic.ROOT_OPERATION_SET_SPAN_TREES)
@@ -87,50 +60,7 @@ object ModelerApp extends App {
       .filterNot((k, v) => Topic.EMPTY_KEY.equalsIgnoreCase(v))
       .to(Topic.ROOT_OPERATION_BPMN_XML)
 
-
     builder.build()
   }
-
-  def preStart(properties: Properties): Unit = {
-
-    val adminClient: AdminClient = AdminClient.create(properties)
-    val newTopics =
-      Seq(
-        new NewTopic(Topic.ROOT_OPERATION_SET_SPAN_TREES, 1, 1),
-        new NewTopic(Topic.ROOT_OPERATION_BPMN_XML, 1, 1)
-      )
-    try {
-      val result = adminClient.createTopics(newTopics.asJava)
-      result.all().get()
-    } catch {
-      case e: ExecutionException =>
-        e.getCause match {
-          case _: TopicExistsException =>
-            println(s"Topics ${newTopics.map(_.name())} already exist")
-        }
-      case e: Throwable => throw e
-    }
-  }
-
-  def addShutdownHook(streams: KafkaStreams, latch: CountDownLatch): Unit = {
-    // attach shutdown handler to catch control-c
-    Runtime.getRuntime.addShutdownHook(new Thread("streams-experiments-shutdown-hook") {
-      override def run(): Unit = {
-        streams.close()
-        latch.countDown()
-      }
-    })
-
-    try {
-      streams.start()
-      latch.await()
-    } catch {
-      case e: Throwable =>
-        System.exit(1)
-    }
-    System.exit(0)
-  }
-
-
 
 }
